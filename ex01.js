2baeef56a72c760e1dad80d96a814ee99d4e55f1
AnaPaula123416 <paula.barco@estudante.ifro.edu.br>
2025-04-03 11:29:43 -0400

    atividade 01 03/04/2025

---
+++ ex01.js
@@ -0,0 +1,33 @@
+//Escreva um programa que declare duas variáveis, “nome” e “idade”, e as
+//imprima em um console em uma frase que diga “Olá, meu nome é [nome]
+//e eu tenho [idade] anos”.
+
+let nome = "Ana Paula";
+let idade = 16;
+
+console.log("Olá meu nome é", nome ,"e eu tenho" ,idade);
+
+//Declare uma string e utilize métodos para converter para maiúsculas e
+//minúsculas.
+let string = "Olá"
+
+lower = string.toLowerCase()
+console.log(lower)
+
+upper = string.toUpperCase()
+console.log(upper)
+
+
+//Declare uma variável e verifique se o tipo dela é number
+
+let text = (typeof num)
+console.log(text)
+let numero = (typeof int)
+console.log(numero)
+
+//Calcule o Índice de Massa Corporal (IMC) utilizando variáveis para altura e
+//peso.
+let peso = 49
+let altura = 1.60
+let imc = peso / (altura * altura)
+console.log(imc)
